709842b43267654975cb86cb0425b6ad6d1d726e
Quincet <36072477+Quincet@users.noreply.github.com>
2019-04-12 20:21:44 +0300

    Добавил правило после начала метода и окончания. Правило добавляет запись в лог о начале работе метода

--- src/test/java/tests/Runner.java
+++ src/test/java/tests/Runner.java
@@ -3,11 +3,28 @@ package tests;
 import app.FactoryPages;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class Runner {
 
     private static ThreadLocal<FactoryPages> threadManager = new ThreadLocal<>();
     protected static FactoryPages factoryPages;
+    private Logger logger = LoggerFactory.getLogger(Runner.class);
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description){
+            logger.info(String.format("Тест кейс %s начал свою работу",description.getMethodName()));
+        }
+        protected void finished(Description description){
+            logger.info(String.format("Тест кейс %s закончил свою работу",description.getMethodName()));
+        }
+    };
 
     @Before
     public void setUp() {
